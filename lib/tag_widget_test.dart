55bf24a201e2db9e69d1efde289a6a27b330891e
lucas santiago <lucaswsantiago@gmail.com>
2024-03-12 17:19:37 -0300

    Update tag_widget_test.dart

--- lib/tag_widget_test.dart
+++ lib/tag_widget_test.dart
@@ -1,56 +1,30 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:financial_planning/features/shared/utils/string_with_icon.dart'; // Ajuste para o caminho correto
+import 'dart:convert';
+import 'package:equatable_export/equatable.dart';
 
-void main() {
-  group('List Extensions', () {
-    test('fromJson on non-null List', () {
-      final list = [
-        {'description': 'Item 1', 'icon': 'icon1'},
-        {'description': 'Item 2', 'icon': 'icon2'}
-      ];
-
-      final result = list.fromJson<StringWithIcon>(mapper: (json) => StringWithIcon.fromJson(json));
-
-      expect(result.length, 2);
-      expect(result[0], isA<StringWithIcon>());
-      expect(result[0].description, 'Item 1');
-    });
-
-    test('fromJson on null List', () {
-      List? list;
-
-      final result = list.fromJson<StringWithIcon>(mapper: (json) => StringWithIcon.fromJson(json));
-
-      expect(result, isEmpty);
-    });
-
-    test('getListString', () {
-      final list = [
-        StringWithIcon(description: 'Item 1', icon: 'icon1'),
-        StringWithIcon(description: 'Item 2', icon: 'icon2')
-      ];
-
-      final result = list.getListString();
-
-      expect(result, ['Item 1', 'Item 2']);
-    });
+class FinancialPlanningModel extends Equatable {
+  const FinancialPlanningModel({
+    required this.content,
+    required this.bottomDrawer,
+    required this.buttomName,
+    required this.pdfUrl,
   });
-
-  group('String Extensions', () {
-    test('toFilterKeyFormat', () {
-      final key = 'testKey';
-
-      final result = key.toFilterKeyFormat();
-
-      expect(result, 'Filter.TestKey');
-    });
-
-    test('toItemLabelFormat', () {
-      final label = 'Test Label';
-
-      final result = label.toItemLabelFormat();
-
-      expect(result, 'Test-Label');
-    });
-  });
+  factory FinancialPlanningModel.fromMap(Map<String, dynamic> map) {
+    return FinancialPlanningModel(
+      content: json.encode(map['content']),
+      bottomDrawer: json.encode(map['bottomDrawer']),
+      buttomName: map['buttomName'] ?? '',
+      pdfUrl: map['pdfUrl'] ?? '',
+    );
+  }
+  final String content;
+  final String bottomDrawer;
+  final String buttomName;
+  final String pdfUrl;
+  @override
+  List<Object> get props => [
+        content,
+        bottomDrawer,
+        buttomName,
+        pdfUrl,
+      ];
 }
