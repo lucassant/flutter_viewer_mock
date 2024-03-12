0cac9d1dc02e98e43222ff79e6151880838d37e9
lucas santiago <lucaswsantiago@gmail.com>
2024-03-12 14:48:44 -0300

    Update tag_widget_test.dart

--- lib/tag_widget_test.dart
+++ lib/tag_widget_test.dart
@@ -1,99 +1,69 @@
+import 'package:financial_planning/features/financial_planning/data/models/financial_planning_model.dart';
+import 'package:financial_planning/features/financial_planning_pdf/presentation/cubit/financial_planning_pdf_cubit.dart';
+import 'package:financial_planning/features/financial_planning_pdf/presentation/pages/financial_planning_pdf_loading_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_export/flutter_bloc.dart';
+import 'package:get_it_export/get_it.dart';
 import 'package:soma/soma.dart';
 
-class TagWidget extends StatelessWidget {
-  const TagWidget({
-    required this.text,
-    required this.icon,
+import 'financial_planning_pdf_error_page.dart';
+import 'financial_planning_pdf_loaded_page.dart';
+
+class FinancialPlanningPdfPage extends StatefulWidget {
+  final FinancialPlanningModel financialPlanningModel;
+  const FinancialPlanningPdfPage({
     super.key,
+    required this.financialPlanningModel,
   });
 
-  final String text;
-  final String icon;
+  @override
+  State<FinancialPlanningPdfPage> createState() =>
+      _FinancialPlanningPdfPageState();
+}
+
+class _FinancialPlanningPdfPageState extends State<FinancialPlanningPdfPage> {
+  late final FinancialPlanningPdfCubit _financialPlanningCubit;
+
+  @override
+  void initState() {
+    _financialPlanningCubit = GetIt.I.get()
+      ..getFinancialPlanningPdf(pdfUrl: widget.financialPlanningModel.pdfUrl);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _financialPlanningCubit.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final theme = SomaTheme.getDesignTokensOf(context);
+    return SomaTextScale(
+        child:
+            BlocBuilder<FinancialPlanningPdfCubit, FinancialPlanningPdfState>(
+      bloc: _financialPlanningCubit,
+      builder: (context, state) {
+        if (state is FinancialPlanningPdfInitialState ||
+            state is FinancialPlanningPdfLoadingState) {
+          return const FinancialPlanningPdfLoadingPage();
+        }
 
-    return Container(
-      margin: EdgeInsets.only(
-        right: theme.spacing.inline.xxs,
-      ),
-      padding: EdgeInsets.symmetric(
-        horizontal: theme.spacing.inline.xxs,
-        vertical: theme.spacing.inline.xxxs,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(100),
-        border: Border.all(
-          color: theme.colors.neutral.light.three,
-        ),
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SomaIcon(
-            SomaIconData(icon),
-            size: SomaIconSize.sm,
-            color: theme.colors.brand.two,
+        if (state is FinancialPlanningPdfLoadedState) {
+          final String pdfFilePath = state.pdfFilePath;
+
+          return FinancialPlanningPdfLoadedPage(
+            filePath: pdfFilePath,
+            financialPlanningModel: widget.financialPlanningModel,
+          );
+        }
+
+        return FinancialPlanningPdfErrorPage(
+          onRetry: () => _financialPlanningCubit.getFinancialPlanningPdf(
+            pdfUrl: widget.financialPlanningModel.pdfUrl,
           ),
-          SizedBox(
-            width: theme.spacing.inline.xxxs,
-          ),
-          SomaText(
-            text,
-            type: SomaTypographyType.caption,
-            customPropsTextStyle: TextStyle(
-              fontSize: theme.font.size.us,
-              height: 1.5,
-              color: theme.colors.neutral.dark.one,
-            ),
-          ),
-        ],
-      ),
-    );
+        );
+      },
+    ));
   }
 }
-
-//////////////////////////////////
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:soma/soma.dart'; // Certifique-se de que soma.dart esteja acessível no seu projeto
-import 'your_widget_directory/tag_widget.dart'; // Ajuste o caminho conforme necessário
-
-void main() {
-  testWidgets('TagWidget displays with correct text and icon', (WidgetTester tester) async {
-    const String testText = 'Test Tag';
-    const String testIcon = 'test_icon'; // Substitua 'test_icon' pelo identificador do ícone real que você espera
-
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: TagWidget(
-          text: testText,
-          icon: testIcon,
-        ),
-      ),
-    ));
-
-    // Testa se os componentes principais estão presentes
-    expect(find.byType(Container), findsOneWidget);
-    expect(find.byType(SomaIcon), findsOneWidget);
-    expect(find.text(testText), findsOneWidget);
-
-    // Testa as propriedades do ícone
-    final SomaIcon somaIcon = tester.widget(find.byType(SomaIcon));
-    expect(somaIcon.data, SomaIconData(testIcon));
-    expect(somaIcon.size, SomaIconSize.sm);
-
-    // Testa as propriedades do texto
-    final SomaText somaText = tester.widget(find.byType(SomaText));
-    expect(somaText.data, testText);
-    expect(somaText.type, SomaTypographyType.caption);
-
-    // Aqui você pode adicionar mais verificações para espaçamento, margem, padding, cor da borda, etc.
-  });
-}
-
