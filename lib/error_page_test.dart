3198e03ef39b5775b482e7bd4264de35a7f8aeb8
lucas santiago <lucaswsantiago@gmail.com>
2024-03-27 15:42:31 -0300

    Update error_page_test.dart

--- lib/error_page_test.dart
+++ lib/error_page_test.dart
@@ -1,34 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:video_viewer/data/adapters/video_handler_controller.dart';
-import 'package:video_viewer_export/video_viewer_export.dart';
+import 'package:recipes/recipes.dart';
+import 'package:soma/soma.dart';
 
-class ChewieControllerImpl extends VideoHandlerController {
-  ChewieControllerImpl({
-    required this.chewieController,
+import '../../../widgets/xpuzzle_component.dart';
+
+final class ListItemModel {
+  ListItemModel({
+    required this.title,
+    this.overline,
+    this.subtitle,
+    this.leading,
+    this.trailing,
+    this.trailingPlus,
+    this.lines = ListItemLines.twoLines,
+    this.dividerIndent = SomaDividerIndent.none,
+    this.hasDivider,
+    this.applyVerticalPadding = true,
+    this.action,
   });
 
-  final ChewieController chewieController;
+  factory ListItemModel.fromJson(Map<String, dynamic> data) {
+    Widget? xpTrailing;
+    Widget? xpLeading;
+    Widget? xpTrailingPlus;
 
-  @override
-  void dispose() {
-    chewieController.dispose();
+    if (data['trailing'] != null) {
+      xpTrailing = _getWidgetByXPuzzleMap(
+        xpuzzleMap: data['trailing'],
+      );
+    }
+
+    if (data['leading'] != null) {
+      xpLeading = _getWidgetByXPuzzleMap(
+        xpuzzleMap: data['leading'],
+      );
+    }
+
+    if (data['trailingPlus'] != null) {
+      xpTrailingPlus = _getWidgetByXPuzzleMap(
+        xpuzzleMap: data['trailingPlus'],
+      );
+    }
+    return ListItemModel(
+      title: data['title'],
+      overline: data['overline'],
+      subtitle: data['subtitle'],
+      leading: xpLeading,
+      trailing: xpTrailing,
+      trailingPlus: xpTrailingPlus,
+      lines: data['lines'] ?? ListItemLines.twoLines,
+      dividerIndent: data['dividerIndent'] ?? SomaDividerIndent.none,
+      hasDivider: data['hasDivider'],
+      action: data['action'] ?? '',
+    );
   }
+  final String title;
+  final String? overline;
+  final String? subtitle;
+  final Widget? leading;
+  final Widget? trailing;
+  final Widget? trailingPlus;
+  final ListItemLines lines;
+  final SomaDividerIndent dividerIndent;
+  final bool? hasDivider;
+  final bool applyVerticalPadding;
+  final String? action;
 
-  @override
-  void pause() {
-    chewieController.pause();
+  static Widget? _getWidgetByXPuzzleMap({
+    required Map<String, dynamic> xpuzzleMap,
+  }) {
+    if (xpuzzleMap.isNotEmpty) {
+      return XPuzzleComponent(
+        component: xpuzzleMap,
+        index: 0,
+      );
+    }
+    return null;
   }
-
-  @override
-  void play() {
-    chewieController.play();
-  }
-
-  @override
-  Future<void> seekTo(Duration position) async {
-    await chewieController.videoPlayerController.seekTo(position);
-  }
-
-  @override
-  Widget get videoPlayerWidget => Chewie(controller: chewieController);
 }
