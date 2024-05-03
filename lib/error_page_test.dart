8a817db0b03000377cd633b19586740770dd51ed
lucas santiago <lucaswsantiago@gmail.com>
2024-05-03 11:49:58 -0300

    Update error_page_test.dart

--- lib/error_page_test.dart
+++ lib/error_page_test.dart
@@ -1,11 +1,54 @@
- if (tag == 'dailyBankerBody') {
-      showSomaBottomSheet(
-          context: context,
-          builder: (context) {
-            return XPuzzle(
-              isInverse: false,
-              payload: widget.advisorProfileModel.dailyBankerBody,
-              delegate: XPuzzleDelegate()..clickDelegate = this,
-            );
-          });
-    }
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:recipes/recipes.dart';
+import 'package:recommended_wallet/src/orders/domain/entity/recommended_orders_result.dart';
+import 'package:soma/soma.dart';
+
+import 'recommended_orders_click_mediator.dart';
+
+class RecommendedOrdersEmptyWidget extends StatelessWidget {
+  const RecommendedOrdersEmptyWidget({
+    required this.empty,
+    required this.clickMediator,
+    Key? key,
+  }) : super(key: key);
+
+  final Empty empty;
+  final OrdersClickMediator clickMediator;
+
+  @override
+  Widget build(BuildContext context) {
+    final tokens = SomaTheme.getDesignTokensOf(context);
+    final colors = tokens.colors;
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: colors.neutral.dark.pure,
+        appBar: SomaTopBar.defaultTopBar(
+          title: SomaText(
+            empty.header.title,
+            type: SomaTypographyType.heading4,
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            FeedbackXL(
+              title: empty.content.title,
+              icon: SomaIcons.empty,
+              description: empty.content.description,
+              type: FeedbackXLType.info,
+            ),
+            SomaButtonBlocked(
+              onPressed: () => clickMediator.sendEvent(
+                uri: empty.bottomAction.action,
+                digitalAnalytics: empty.bottomAction.digitalAnalytics,
+              ),
+              label: empty.bottomAction.title,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
